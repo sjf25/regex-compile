88caed6d7e669c16be9f29be0603b8b019c7c86e
sjf25 <sohaibjf@gmail.com>
2019-02-23 17:04:14 -0600

    got basic parser working

--- Runner.scala
+++ Runner.scala
@@ -6,6 +6,7 @@ object Runner {
   def main(args: Array[String]) {
     
     // email regex example
+    /*
     val aToZ = Regex.mergeUlists(Regex.range('a', 'z'),
       Regex.range('A', 'Z'))
     val aToZNum = Regex.mergeUlists(Regex.range('0', '9'), aToZ)
@@ -19,23 +20,24 @@ object Runner {
     val (states, trans, accept, start) = reg.dfa(alpha)
     val cCode = new DFA(states, trans, accept, start).toC()
     println(cCode)
+    */
     /*
     println(s"$states\n$accept")
     println("TRANS:")
     trans.foreach(v => println(s"$v"))
     */
-    /*
     if(args.length != 1) {
       println("incorrect usage")
       return
     }
     val regexStr = args(0)
-    val parser = new ParseRegex()
+    //val parser = new ParseRegex()
     //val tmp = parser.parse(parser.test, regexStr)
-    val tmp = parser.parse(parser.regex, regexStr)
+    //val tmp = parser.parse(parser.regex, regexStr)
+    //val tmp = parser.parseAll(parser.regex, regexStr)
     //val tmp = parser.parse(regexStr)
     //val tmp = ParseRegex.parseRegex(regexStr)
+    val tmp = ParseRegex.parse(regexStr)
     println(s"$tmp")
-    */
   }
 }
